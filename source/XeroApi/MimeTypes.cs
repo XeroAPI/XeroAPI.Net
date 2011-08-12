4b147cd6977776f7e33e20f9c26a81e76e8e30d7
D4 <d4@xero.com>
2011-08-12 15:12:08 +1200

    Add list of mime types used in Xero API

---
+++ source/XeroApi/MimeTypes.cs
@@ -0,0 +1,21 @@
+﻿namespace XeroApi
+{
+    public static class MimeTypes
+    {
+
+        public const string TextXml = "text/xml";
+        public const string TextCsv = "text/csv";
+
+        public const string ApplicationPdf = "application/pdf";
+        public const string ApplicationJson = "application/json";
+
+        public const string ImageTiff = "image/tiff";
+        public const string ImagePng = "image/png";
+        public const string ImageGif = "image/gif";
+        public const string ImageJpeg = "image/jpeg";
+        public const string ImageBmp = "image/bmp";
+
+        public const string Unknown = "application/octet-stream";
+
+    }
+}
