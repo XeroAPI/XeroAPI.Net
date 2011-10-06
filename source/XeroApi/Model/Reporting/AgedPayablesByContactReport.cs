762ed0518fb676e1667426852cee4d0c1881d763
D4 <d4@xero.com>
2011-10-07 12:41:20 +1300

    Add contactId parameter to AgedPayablesByContactReport and AgedReceivablesByContactReport classes

--- source/XeroApi/Model/Reporting/AgedPayablesByContactReport.cs
+++ source/XeroApi/Model/Reporting/AgedPayablesByContactReport.cs
@@ -8,6 +8,7 @@ namespace XeroApi.Model.Reporting
 {
     public class AgedPayablesByContactReport : DynamicReportBase
     {
+        private readonly Guid _contactId;
         private readonly DateTime? _date;
         private readonly DateTime? _fromDate; 
         private readonly DateTime? _toDate;
@@ -16,11 +17,13 @@ namespace XeroApi.Model.Reporting
         /// <summary>
         /// Initializes a new instance of the <see cref="AgedPayablesByContactReport"/> class.
         /// </summary>
+        /// <param name="contactId">The ContactID of the contact to filter on.</param>
         /// <param name="date">Shows payments up to this date. Defaults to end of the current month</param>
         /// <param name="fromDate">Show all payable invoices from this date for contact</param>
         /// <param name="toDate">Show all payable invoices to this date for the contact</param>
-        public AgedPayablesByContactReport(DateTime? date = null, DateTime? fromDate = null, DateTime? toDate = null)
+        public AgedPayablesByContactReport(Guid contactId, DateTime? date = null, DateTime? fromDate = null, DateTime? toDate = null)
         {
+            _contactId = contactId;
             _date = date;
             _toDate = toDate;
             _fromDate = fromDate;
@@ -32,6 +35,8 @@ namespace XeroApi.Model.Reporting
         /// <param name="queryStringParams">The query string params.</param>
         internal override void GenerateQuerystringParams(NameValueCollection queryStringParams)
         {
+            queryStringParams.Add("contactID", _contactId.ToString());
+
             if (_fromDate.HasValue)
                 queryStringParams.Add("fromDate", _fromDate.Value.ToString(ReportDateFormatString));
 
