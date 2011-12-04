49e61f9a294258bdfc12ff4305706fb4ca57c123
Dan Barratt <d4@xero.com>
2011-12-05 10:04:38 +1300

    Add method comments

--- source/XeroApi/Integration/IApiQueryDescription.cs
+++ source/XeroApi/Integration/IApiQueryDescription.cs
@@ -5,9 +5,28 @@ namespace XeroApi.Integration
 {
     public interface IApiQueryDescription
     {
+        /// <summary>
+        /// Gets the element name, used to construct the request url
+        /// </summary>
+        /// <value>The name of the element.</value>
         string ElementName { get; }
+
+        /// <summary>
+        /// Gets the element id, used to construct the request url
+        /// </summary>
+        /// <value>The element id.</value>
         string ElementId { get; }
+
+        /// <summary>
+        /// Gets the date used to populate the If-Modified-Since http header.
+        /// </summary>
+        /// <value>The updated since date.</value>
         DateTime? UpdatedSinceDate { get; }
+
+        /// <summary>
+        /// Gets the query string parameter collection.
+        /// </summary>
+        /// <value>The query string params.</value>
         NameValueCollection QueryStringParams { get; }
     }
 }
