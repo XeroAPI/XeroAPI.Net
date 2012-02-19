68030a97b6c4d370b514f68a7961b8de80fcbc9c
unknown <daniel.barratt@.inside.xero.com>
2012-02-20 09:40:22 +1300

    Add XeroApi.VBTests project

---
+++ source/XeroApi.VBTests/ApiQueryTests.vb
@@ -0,0 +1,20 @@
+﻿Imports NUnit.Framework
+Imports XeroApi.Model
+
+<TestFixture()>
+Public Class ApiQueryTests
+
+    <Test()>
+    Public Sub it_can_query_root_endpoint()
+
+        Dim integrationProxy As New StubIntegrationProxy()
+        Dim repository As New Repository(integrationProxy)
+
+        Dim organisations As List(Of Organisation) = repository.Organisations.ToList()
+
+        Assert.AreEqual(0, organisations.Count)
+        Assert.AreEqual("Organisation", integrationProxy.LastQueryDescription.ElementName)
+
+    End Sub
+
+End Class
---
+++ source/XeroApi.VBTests/Stubs/StubIntegrationProxy.vb
@@ -0,0 +1,57 @@
+﻿Imports System.IO
+Imports XeroApi.Integration
+Imports XeroApi.Model
+
+Public Class StubIntegrationProxy
+    Implements IIntegrationProxy
+
+    
+    Public Function FindElements(apiQueryDescription As IApiQueryDescription) As String Implements IIntegrationProxy.FindElements
+        LastQueryDescription = apiQueryDescription
+        Return GenerateSampleResponseXml(apiQueryDescription.ElementName)
+    End Function
+
+
+    Public Function FindAttachments(endpointName As String, itemId As String) As String Implements IIntegrationProxy.FindAttachments
+        Return GenerateSampleResponseXml(endpointName)
+    End Function
+
+
+    Public Function FindOne(endpointName As String, itemId As String, acceptMimeType As String) As Byte() Implements IIntegrationProxy.FindOne
+        Return Nothing
+    End Function
+
+
+    Public Function FindOneAttachment(endpointName As String, itemId As String, attachmentIdOrFileName As String) As Stream Implements IIntegrationProxy.FindOneAttachment
+        Return Stream.Null
+    End Function
+
+
+    Public Function UpdateOrCreateElements(endpointName As String, body As String) As String Implements IIntegrationProxy.UpdateOrCreateElements
+        Return GenerateSampleResponseXml(endpointName)
+    End Function
+
+
+    Public Function UpdateOrCreateAttachment(endpointName As String, itemId As String, attachment As Attachment) As String Implements IIntegrationProxy.UpdateOrCreateAttachment
+        Return GenerateSampleResponseXml(endpointName)
+    End Function
+
+
+    Public Function CreateElements(endpointName As String, body As String) As String Implements IIntegrationProxy.CreateElements
+        Return GenerateSampleResponseXml(endpointName)
+    End Function
+
+
+    Public Function CreateAttachment(endpointName As String, itemId As String, attachment As Attachment) As String Implements IIntegrationProxy.CreateAttachment
+        Return GenerateSampleResponseXml(endpointName)
+    End Function
+
+    Private Function GenerateSampleResponseXml(elementName As String) As String
+
+        Return "<Response><Id>" & Guid.NewGuid().ToString & "</Id><Status>OK</Status><ProviderName>NullIntegrationProxy</ProviderName><DateTimeUTC>" & DateTime.UtcNow.ToString("s") & "</DateTimeUTC><" & elementName & "s" & " /></Response>"
+
+    End Function
+
+    Public LastQueryDescription As IApiQueryDescription = Nothing
+
+End Class
