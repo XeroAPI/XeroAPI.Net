4270fcc4fc29fe90653c51a8459ea722780dc288
unknown <daniel.barratt@.inside.xero.com>
2012-03-30 12:29:30 +1300

    Ad ToString() method for LineItem

--- source/XeroApi/Model/LineItem.cs
+++ source/XeroApi/Model/LineItem.cs
@@ -21,6 +21,11 @@ namespace XeroApi.Model
         public string ItemCode { get; set; }
 
         public decimal? Quantity { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("LineItem:{0}", Description ?? ItemCode);
+        }
     }
 
     public class LineItems : ModelList<LineItem>
