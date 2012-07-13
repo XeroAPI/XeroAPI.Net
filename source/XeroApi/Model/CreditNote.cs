a98c675017c1029a535f6f0a141eab91126fd8a9
MatthewSteeples <matthew@mercuryit.co.uk>
2012-07-13 16:53:08 +0100

    Create a common base class for CreditNotes and Invoices

--- source/XeroApi/Model/CreditNote.cs
+++ source/XeroApi/Model/CreditNote.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace XeroApi.Model
 {
-    public class CreditNote : EndpointModelBase
+    public class CreditNote : TradingTransaction
     {
         [ItemId]
         public Guid CreditNoteID { get; set; }
@@ -10,42 +10,8 @@ namespace XeroApi.Model
         [ItemNumber]
         public string CreditNoteNumber { get; set; }
 
-        [ItemUpdatedDate]
-        public DateTime? UpdatedDateUTC { get; set; }
-
-        public string Type { get; set; }
-
-        public string Reference { get; set; }
-        
-        public bool? SentToContact { get; set; }
-
         public decimal? AppliedAmount { get; set; }
 
-        public decimal? CurrencyRate { get; set; }
-
-        public Contact Contact { get; set; }
-
-        public DateTime? Date { get; set; }
-
-        public DateTime? DueDate { get; set; }
-
-        public virtual Guid? BrandingThemeID { get; set; }
-
-        public virtual string Status { get; set; }
-
-        public LineAmountType LineAmountTypes { get; set; }
-
-        public virtual LineItems LineItems { get; set; }
-
-        public virtual decimal? SubTotal { get; set; }
-
-        public virtual decimal? TotalTax { get; set; }
-
-        public virtual decimal? Total { get; set; }
-
-        public virtual string CurrencyCode { get; set; }
-
-        public DateTime? FullyPaidOnDate { get; set; }
     }
     
     public class CreditNotes : ModelList<CreditNote>
