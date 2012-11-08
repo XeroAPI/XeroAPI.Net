d7b2bd8af1383592fa9748e3d439eaccc08dcf25
Dan Barratt <d4@xero.com>
2012-11-09 11:23:16 +1300

    Attachment: Rename Filename --> FileName (case sensitive)

--- source/XeroApi/Model/Attachment.cs
+++ source/XeroApi/Model/Attachment.cs
@@ -16,7 +16,7 @@ namespace XeroApi.Model
             if (fileInfo == null) { throw new ArgumentNullException("fileInfo");}
             if (!fileInfo.Exists) { throw new FileNotFoundException("The file could not be found", fileInfo.FullName);}
 
-            Filename = fileInfo.Name;
+            FileName = fileInfo.Name;
             ContentLength = (int)fileInfo.Length;
             MimeType = MimeTypes.GetMimeType(fileInfo);
             ContentStream = fileInfo.OpenRead();
@@ -27,7 +27,7 @@ namespace XeroApi.Model
             if (content == null) { throw new ArgumentNullException("content"); }
             if (string.IsNullOrEmpty(filename)) { throw new ArgumentNullException("filename"); }
 
-            Filename = filename;
+            FileName = filename;
             MimeType = MimeTypes.GetMimeType(filename);
             ContentStream = content;
         }
@@ -37,7 +37,14 @@ namespace XeroApi.Model
         public Guid? AttachmentID { get; set; }
 
         [ItemNumber]
-        public string Filename { get; set; }
+        public string FileName { get; set; }
+        
+        [Obsolete("Use FileName instead (different case)")]
+        public string Filename
+        {
+            get { return FileName; }
+            set { FileName = value; }
+        }
 
         public string MimeType { get; set; }
 
