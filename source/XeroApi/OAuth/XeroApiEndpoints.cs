c1fdfa7d20f4171267530d15a2eb27c5fbe33469
unknown <Richard.Rowley@RichardR-Station.inside.xero.com>
2013-04-23 10:47:43 +1200

    Remove the local urls

--- source/XeroApi/OAuth/XeroApiEndpoints.cs
+++ source/XeroApi/OAuth/XeroApiEndpoints.cs
@@ -4,7 +4,6 @@ namespace XeroApi.OAuth
 {
     public class XeroApiEndpoints
     {
-        /*
         public static Uri UserAuthorizeUri = new Uri("https://api.xero.com/oauth/Authorize");
 
         public static Uri PublicRequestTokenUri = new Uri("https://api.xero.com/oauth/RequestToken");
@@ -14,16 +13,6 @@ namespace XeroApi.OAuth
         public static Uri PartnerAccessTokenUri = new Uri("https://api-partner.network.xero.com/oauth/AccessToken");
 
         public static Uri PublicBaseEndpointUri = new Uri("https://api.xero.com/");
-        public static Uri PartnerBaseEndpointUri = new Uri("https://api-partner.network.xero.com/");*/
-
-        public static Uri UserAuthorizeUri = new Uri("http://api.web/oauth/Authorize");
-        
-        public static Uri PublicRequestTokenUri = new Uri("http://api.web/oauth/RequestToken");
-        public static Uri PublicAccessTokenUri = new Uri("http://api.web/oauth/AccessToken");        
-        public static Uri PublicBaseEndpointUri = new Uri("http://api.web/");
-
-        public static Uri PartnerRequestTokenUri = new Uri("http://api-partner.web/oauth/RequestToken");
-        public static Uri PartnerAccessTokenUri = new Uri("http://api-partner.web/oauth/AccessToken");
-        public static Uri PartnerBaseEndpointUri = new Uri("http://api-partner.web/");
+        public static Uri PartnerBaseEndpointUri = new Uri("https://api-partner.network.xero.com/");        
     }
 }
